758d8499413073a67acec3852bfb94a838ace423
sceptross <up201304205@fe.up.pt>
2017-07-07 02:35:49 +0100

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,68 @@
+const DEF_ARRAY_POS = 0;
+const USE_ARRAY_POS = 1;
+const PRED_ARRAY_POS = 2;
+const SUCC_ARRAY_POS = 3;
+const IN_ARRAY_POS = 4;
+const OUT_ARRAY_POS = 5;
+
+
+$(document).ready(function(){
+    var nRows=1;
+    $("td input").focusout(function() {
+        var re = new RegExp($(this).attr("pattern"));
+        if(!re.test($(this).val()))
+            $(this).css('border-color', 'red');
+        else
+            $(this).css('border-color', '');
+    });
+    $("#add_row").click(function(){
+        $('#addr'+nRows).html("<td>"+ (nRows+1) +"</td><td><input name='def"+nRows+"' type='text' placeholder='ex.: var1, var2, var3, var4' class='form-control input-md' pattern='^([A-Z|a-z][A-Z|a-z|0-9|_]*)( *, *[A-Z|a-z][A-Z|a-z|0-9|_]*)*$'/> </td><td><input  name='use"+nRows+"' type='text' placeholder='ex.: var1, var2, var3, var4'  class='form-control input-md' pattern='^/([A-Z|a-z][A-Z|a-z|0-9|_]*)( *, *[A-Z|a-z][A-Z|a-z|0-9|_]*)*/$'></td><td><input  name='pred"+nRows+"' type='text' placeholder='ex.: 1, 2, 3, 4'  class='form-control input-md' pattern='^/[0-9]+( *, *[0-9]+)*/$'></td><td><input  name='succ"+nRows+"' type='text' placeholder='ex.: 1, 2, 3, 4'  class='form-control input-md' pattern='^/[0-9]+( *, *[0-9]+)*/$'></td>");
+        $('#input_table').append('<tr id="addr'+(nRows+1)+'"></tr>');
+        nRows++;
+    });
+    $("#delete_row").click(function(){
+        if(nRows>1){
+            $("#addr"+(nRows-1)).html('');
+            nRows--;
+            console.log(nRows);
+        }
+    });
+    $("#submit").click(function() {
+        var table = document.getElementById("input_table");
+        var nodes = [];
+        for(var row = 1; row <= nRows; ++row) {
+            var line = [];
+            for (var j = 1; j < table.rows[row].cells.length; ++j)
+                line.push(table.rows[row].cells[j].getElementsByTagName("input")[0].value);
+            nodes.push(line)
+        }
+        var iterations = generate_table(nodes);
+
+        var output_table = $('#output_table');
+        output_table.html('');
+        output_table.append('<thead><tr><th colspan="3"></th></tr></thead><tbody><tr></tr></tbody>');
+        var first_row = output_table.find('thead').find('tr');
+        var table_body = output_table.find('tbody');
+        var second_row = table_body.find('tr');
+        second_row.append('<td>Node</td>');
+        second_row.append('<td>use</td>');
+        second_row.append('<td>def</td>');
+
+        for(let i = 0; i < nodes.length; ++i) {
+            table_body.append("<tr> <td>" + (i+1) + "</td><td>" + nodes[i][USE_ARRAY_POS] +
+                "</td><td>" + nodes[i][DEF_ARRAY_POS] + "</td> </tr>");
+        }
+
+        for(let i = 0; i < iterations.length; ++i) {
+            first_row.append("<th colspan='2'>Iteration " + (i+1) + "</th>");
+            second_row.append("<td>in</td>");
+            second_row.append("<td>out</td>");
+            for (let j = 0; j < nodes.length; ++j) {
+                table_body.find('tr').eq(j+1).append("<td>" + iterations[i][j][0] + "</td>");
+                table_body.find('tr').eq(j+1).append("<td>" + iterations[i][j][1] + "</td>");
+            }
+        }
+        $('#output').show();
+    })
+
+});
---
+++ js/liveness.js
@@ -0,0 +1,90 @@
+function generate_table(parsed_nodes) {
+    var nodes = separate_sets(parsed_nodes);
+    var iterations = [];
+    var last_iteration = [];
+    while (!compare_arrays(last_iteration, nodes)) {
+        last_iteration = JSON.parse(JSON.stringify(nodes));
+        iterations.push([]);
+        for (var i = 0; i < nodes.length; ++i) {
+            nodes = compute_node_in_out(nodes, i);
+            iterations[iterations.length-1].push([nodes[i][IN_ARRAY_POS], nodes[i][OUT_ARRAY_POS]]);
+        }
+    }
+    return iterations;
+}
+
+function separate_sets(nodes) {
+    var separated_nodes = [];
+    for (var i = 0; i < nodes.length; ++i) {
+        var node = [];
+        for (var j = 0; j < nodes[i].length; ++j) {
+            if (nodes[i][j] === "") {
+                node.push([]);
+                continue;
+            }
+            node.push(nodes[i][j].split(",").map((item) => item.trim()));
+        }
+        node.push([], []);
+        separated_nodes.push(node);
+    }
+    return separated_nodes;
+}
+
+function compute_node_in_out(nodes, node_id) {
+    var in_array = add_arrays(nodes[node_id][USE_ARRAY_POS], subtract_arrays(nodes[node_id][OUT_ARRAY_POS], nodes[node_id][DEF_ARRAY_POS]));
+    var out_array = [];
+    for (var i = 0; i < nodes[node_id][SUCC_ARRAY_POS].length; ++i) {
+        var succ = parseInt(nodes[node_id][SUCC_ARRAY_POS][i])-1;
+        out_array = add_arrays(out_array, nodes[succ][IN_ARRAY_POS]);
+    }
+    nodes[node_id][IN_ARRAY_POS] = in_array;
+    nodes[node_id][OUT_ARRAY_POS] = out_array;
+    return nodes;
+}
+
+function subtract_arrays(array1, array2) {
+    return array1.filter( function (el) {
+        return !array2.includes(el);
+    })
+}
+
+function add_arrays(array1, array2) {
+    var ret = array1.concat(array2);
+    for (var i = 0; i < ret.length; ++i) {
+        for(var j=i+1; j< ret.length; ++j) {
+            if(ret[i] === ret[j])
+                ret.splice(j--, 1);
+        }
+    }
+    return ret;
+}
+
+// Note: Thanks to SamyBencherif for this implementation.
+// https://jsfiddle.net/SamyBencherif/8352y6yw/
+function compare_arrays(array1, array2) {
+    // if the other array is a falsy value, return
+    if (!array1 || !array2)
+        return false;
+
+    // compare lengths - can save a lot of time
+    if (array1.length !== array2.length)
+        return false;
+
+    for (var i = 0, l=array1.length; i < l; i++) {
+        // Check if we have nested arrays
+        if (array1[i] instanceof Array && array2[i] instanceof Array) {
+            // recurse into the nested arrays
+            if (!compare_arrays(array1[i],array2[i])){
+                console.log("Teste1")
+                return false;
+            }
+        }
+        else if (array1[i] !== array2[i]) {
+            // Warning - two different object instances will never be equal: {x:20} != {x:20}
+            console.log("Teste2")
+            return false;
+        }
+    }
+    console.log("Teste")
+    return true;
+}
